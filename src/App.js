a4e54430fcdac3c175427505d7e81181ca9f0eaf
Luke Byrne <byrneluke1999@users.noreply.github.com>
2020-07-23 17:46:04 +0100

    Set Up + Now Playing feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* 
+  Instead of manualling makng an API call I am going to use a library which abstracts pretty much every API call I could use.
+  It was developed by a Spotify engineer, hence why I'm using.
+*/
+import SpotifyWebApi from "spotify-web-api-js";
+const spotifyApi = new SpotifyWebApi();
+
+class App extends React.Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    console.log(params);
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: "Not Checked", albumArt: "" },
+    };
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <a href="http://localhost:8888"> Login to Spotify </a>
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+        </div>
+        {this.state.loggedIn && (
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
