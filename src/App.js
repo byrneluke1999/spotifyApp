ac1708042bcf1de531bef989010f2d89a9c4b029
Luke Byrne <byrneluke1999@users.noreply.github.com>
2020-08-14 22:31:07 +0100

    Revert "Setting up somethign new with app routing & diff API stuff"
    This reverts commit 14f86d3372a244203bff8b506e90c20bbea0b1c4.
    change to old version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,192 @@
 import React from "react";
 import "./App.css";
-import "./index.css";
-import { HashRouter, Switch, Route } from "react-router-dom";
 
-import stats from "./components/stats";
-import login from "./components/login";
+// styling
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function App() {
-  return (
-    <HashRouter>
+//Instead of manualling makng an API call I am going to use a library which abstracts pretty much every API call I could use.
+//It was developed by a Spotify engineer, hence why I'm using.
+
+import SpotifyWebApi from "spotify-web-api-js";
+const spotifyApi = new SpotifyWebApi();
+
+class App extends React.Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    console.log(params);
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      user: { name: "" },
+      nowPlaying: {
+        name: "Not Checked",
+        artist: "Not Checked",
+        albumArt: "img",
+      },
+      topTracks: [],
+      topArtists: [],
+    };
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      if (response === "") {
+        this.setState({
+          nowPlaying: {
+            name: "You're not listening to anything right now",
+            artist: "Nobody",
+            albumArt: "",
+          },
+        });
+      } else {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            artist: response.item.artists[0].name,
+            albumArt: response.item.album.images[0].url,
+          },
+        });
+      }
+    });
+  }
+
+  getTopTracks() {
+    spotifyApi
+      .getMyTopTracks()
+      .then((response) => this.setState({ topTracks: response.items }));
+  }
+
+  getTopArtists() {
+    spotifyApi
+      .getMyTopArtists()
+      .then((response) => this.setState({ topArtists: response.items }));
+  }
+
+  getUserDetails() {
+    spotifyApi.getMe().then((response) =>
+      this.setState({
+        user: {
+          name: response.display_name,
+        },
+      })
+    );
+  }
+
+  render() {
+    this.getUserDetails();
+    this.getNowPlaying();
+    let userNav;
+    if (this.state.loggedIn) {
+      userNav = (
+        <Nav.Item className="mr-auto" style={{ color: "#ee6c4d" }}>
+          Signed in as: {this.state.user.name}
+        </Nav.Item>
+      );
+    } else {
+      userNav = (
+        <Nav.Link
+          href="http://localhost:8888"
+          className="mr-auto"
+          style={{ color: "#ee6c4d" }}
+        >
+          Login to Spotify
+        </Nav.Link>
+      );
+    }
+
+    return (
       <div className="App">
-        <Switch>
-          <Route path="/stats" component={stats} />
-          <Route exact path="/" component={login} />
-          <Route path="" component={login} />
-        </Switch>
+        <Navbar collapseOnSelect bg="dark" variant="dark">
+          <Navbar.Brand href="#home" style={{ color: "#ee6c4d" }}>
+            SpotifyStats
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Item style={{ color: "white" }}> Now Playing: </Nav.Item>
+              <img
+                src={this.state.nowPlaying.albumArt}
+                style={{ width: 50, height: 50, marginTop: -7 }}
+              />
+              <Nav.Item style={{ color: "#ee6c4d" }}>
+                {this.state.nowPlaying.name}
+              </Nav.Item>
+              <Nav.Item style={{ color: "white" }}>by</Nav.Item>
+              <Nav.Item style={{ color: "#ee6c4d" }}>
+                {this.state.nowPlaying.artist}{" "}
+              </Nav.Item>
+            </Nav>
+
+            <Nav>{userNav}</Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <CardGroup>
+          <Card style={{ width: "18rem", borderColor: "#ee6c4d" }} bg="dark">
+            <Card.Img variant="top" />
+            <Card.Body>
+              <Card.Title style={{ color: "#ee6c4d" }}></Card.Title>
+              <Card.Text style={{ color: "white" }}>
+                The last 6 months
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush"></ListGroup>
+          </Card>
+          <Card style={{ width: "18rem", borderColor: "#ee6c4d" }} bg="dark">
+            <Card.Img variant="top" />
+            <Card.Body>
+              <Card.Title style={{ color: "#ee6c4d" }}></Card.Title>
+              <Card.Text style={{ color: "white" }}>
+                The last 6 months
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              {this.state.topArtists.map((artist) => (
+                <ListGroup.Item
+                  as="li"
+                  key={artist.id}
+                  style={{ backgroundColor: "#484d53", color: "white" }}
+                >
+                  {artist.name}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Card>
+        </CardGroup>
+        {this.state.loggedIn && (
+          <Button
+            variant="dark"
+            onClick={() => {
+              this.getTopTracks();
+              this.getTopArtists();
+            }}
+          >
+            Check your top Tracks and Artists
+          </Button>
+        )}
       </div>
-    </HashRouter>
-  );
+    );
+  }
 }
 
 export default App;
